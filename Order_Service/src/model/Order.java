b599baf3a04667fbfabf631d76d5011399b8593d
ShalaniMW <shalawije@gmail.com>
2021-04-19 23:45:23 +0530

    Implement database connection

--- Order_Service/src/model/Order.java
+++ Order_Service/src/model/Order.java
@@ -1,5 +1,23 @@
 package model;
 
+import java.sql.*; 
+
 public class Order {
+	
+	//Database connection
+		private Connection connect() 
+		 { 
+		 Connection con = null; 
+		 try
+		 { 
+		 Class.forName("com.mysql.jdbc.Driver"); 
+		 
+		 //DBServer/DBName, username, password 
+		 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/orderdb", "root", "1233"); 
+		 } 
+		 catch (Exception e) 
+		 {e.printStackTrace();} 
+		 return con; 
+		 } 
 
 }
