c3677e77c5c4495f7abf95fab1e3d9091c67d272
ShalaniMW <shalawije@gmail.com>
2021-04-20 00:00:21 +0530

    Implement methods in the server-model

--- Order_Service/src/model/Order.java
+++ Order_Service/src/model/Order.java
@@ -12,12 +12,156 @@ public class Order {
 		 { 
 		 Class.forName("com.mysql.jdbc.Driver"); 
 		 
-		 //DBServer/DBName, username, password 
+	     //DBServer/DBName, username, password 
 		 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/orderdb", "root", "1233"); 
 		 } 
 		 catch (Exception e) 
 		 {e.printStackTrace();} 
 		 return con; 
 		 } 
+		
+	//insertOrder method
+		public String insertOrder(String productID, String productName, String buyerName, String buyerPhone,String buyerMail) 
+		 { 
+		 String output = ""; 
+		 try
+		 { 
+		 Connection con = connect(); 
+		 if (con == null) 
+		 {return "Error while connecting to the database for inserting."; } 
+		 // create a prepared statement
+		 String query = " insert into orders (`orderID`,`productID`,`productName`,`buyerName`,`buyerPhone`,`buyerMail`)"
+		 + " values (?, ?, ?, ?, ?, ?)"; 
+		 PreparedStatement preparedStmt = con.prepareStatement(query); 
+		 // binding values
+		 preparedStmt.setInt(1, 0); 
+		 preparedStmt.setString(2, productID); 
+		 preparedStmt.setString(3, productName); 
+		 preparedStmt.setString(4, buyerName); 
+		 preparedStmt.setString(5, buyerPhone);
+		 preparedStmt.setString(6, buyerMail); 
+		// execute the statement
+		 preparedStmt.execute(); 
+		 con.close(); 
+		 output = "Inserted successfully"; 
+		 } 
+		 catch (Exception e) 
+		 { 
+		 output = "Error while inserting the item."; 
+		 System.err.println(e.getMessage()); 
+		 } 
+		 return output; 
+		 } 
+		
+	//readOrder method
+		public String readOrders() 
+		 { 
+		 String output = ""; 
+		 try
+		 { 
+		 Connection con = connect(); 
+		 if (con == null) 
+		 {return "Error while connecting to the database for reading."; } 
+		 // Prepare the html table to be displayed
+		 output = "<table border='1'><tr><th>ProductID</th><th>Product Name</th>" +
+		 "<th>Name</th>" + 
+		 "<th>Phone Number</th>" +
+		 "<th>Email</th>" +
+		 "<th>Update</th><th>Remove</th></tr>"; 
+		 
+		 String query = "select * from orders"; 
+		 Statement stmt = con.createStatement(); 
+		 ResultSet rs = stmt.executeQuery(query); 
+		 // iterate through the rows in the result set
+		 while (rs.next()) 
+		 { 
+		 String orderID = Integer.toString(rs.getInt("orderID")); 
+		 String productID = rs.getString("productID"); 
+		 String productName = rs.getString("productName"); 
+		 String buyerName = rs.getString("buyerName");
+		 String buyerPhone = rs.getString("buyerPhone");
+		 String buyerMail = rs.getString("buyerMail"); 
+		 // Add into the html table
+		 output += "<tr><td>" + productID + "</td>"; 
+		 output += "<td>" + productName + "</td>"; 
+		 output += "<td>" + buyerName + "</td>"; 
+		 output += "<td>" + buyerPhone + "</td>";
+		 output += "<td>" + buyerMail + "</td>"; 
+		 // buttons
+		 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"+ "<td><form method='post' action='items.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+		 + "<input name='orderID' type='hidden' value='" + orderID 
+		 + "'>" + "</form></td></tr>"; 
+		 } 
+		 con.close(); 
+		 // Complete the html table
+		 output += "</table>"; 
+		 } 
+		 catch (Exception e) 
+		 { 
+		 output = "Error while reading the items."; 
+		 System.err.println(e.getMessage()); 
+		 } 
+		 return output; 
+		 } 
+		
+	//updateOrder method
+		public String updateOrder(String ID,String productID, String productName, String buyerName, String buyerPhone,String buyerMail)
+		{ 
+			 String output = ""; 
+			 try
+			 { 
+			 Connection con = connect(); 
+			 if (con == null) 
+			 {return "Error while connecting to the database for updating."; } 
+			 // create a prepared statement
+			 String query = "UPDATE orders SET productID=?,productName=?,buyerName=?,buyerPhone=?,buyerMail=?  WHERE orderID=?"; 
+			 PreparedStatement preparedStmt = con.prepareStatement(query); 
+			 // binding values
+			 preparedStmt.setString(1, productID); 
+			 preparedStmt.setString(2, productName); 
+			 preparedStmt.setString(3, buyerName); 
+			 preparedStmt.setString(4, buyerPhone);
+			 preparedStmt.setString(5, buyerMail);
+			 preparedStmt.setInt(5, Integer.parseInt(ID)); 
+			 // execute the statement
+			 preparedStmt.execute(); 
+			 con.close(); 
+			 output = "Updated successfully"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+			 output = "Error while updating the item."; 
+			 System.err.println(e.getMessage()); 
+			 } 
+			 return output; 
+			 } 
+		
+		//deleteOrder method
+			public String deleteOrder(String orderID) 
+			 { 
+			 String output = ""; 
+			 try
+			 { 
+			 Connection con = connect(); 
+			 if (con == null) 
+			 {return "Error while connecting to the database for deleting."; } 
+			 // create a prepared statement
+			 String query = "delete from orders where orderID=?"; 
+			 PreparedStatement preparedStmt = con.prepareStatement(query); 
+			 // binding values
+			 preparedStmt.setInt(1, Integer.parseInt(orderID)); 
+			 // execute the statement
+			 preparedStmt.execute(); 
+			 con.close(); 
+			 output = "Deleted successfully"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+			 output = "Error while deleting the item."; 
+			 System.err.println(e.getMessage()); 
+			 } 
+			 return output; 
+			 } 
+
 
 }
